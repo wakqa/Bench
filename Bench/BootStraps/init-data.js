1714aee6dcba6baa8ff0c34ee856a22cdeed7386
wakqa <soufiane.tigrioui@4d.com>
2013-11-15 12:18:46 +0000

    Update #4

--- Bench/BootStraps/init-data.js
+++ Bench/BootStraps/init-data.js
@@ -1,4 +1,25 @@
 ﻿var total = 1000;
 var totalLarge = 100000;
 var raz = false;
+
+//	Generate data 
+
 DataGenUtils.generateData(ds.BasicReadLarge,totalLarge,raz);
+
+DataGenUtils.generateData(ds.BasicCreate,total,raz);
+
+DataGenUtils.genarateData(ds.BasicRemove,total,raz);
+
+DateGenUtils.generateData(ds.BasicReadSmall,total,raz);
+
+DateGenUtils.generateData(ds.BasicLink,total,raz);
+
+DateGenUtils.generateData(ds.BasicUpdate,total,raz);
+
+DateGenUtils.generateData(ds.BasicClass,total,raz);
+
+DateGenUtils.generateData(ds.ComplexReadLarge,totalLarge,raz);
+
+DateGenUtils.generateData(ds.ComplexReadSmall,total,raz);
+
+DateGenUtils.generateData(ds.Indexes,totalLarge,raz);
